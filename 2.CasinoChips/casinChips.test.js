3c4525d089931907f85306965392b3069c760b2b
Vicky <kosenchiha@gmail.com>
2020-01-14 21:50:44 +0000

    Add test cases

--- 2.CasinoChips/casinChips.test.js
+++ 2.CasinoChips/casinChips.test.js
@@ -1,5 +1,13 @@
 const calculateDays = require("./casinoChips");
 
-test("test", () => {
-  expect(calculateDays([])).toEqual(1);
+test("calculateDays", () => {
+  expect(calculateDays([1, 1, 1])).toEqual(1);
+});
+
+test("calculateDays", () => {
+  expect(calculateDays([1, 8, 1])).toEqual(2);
+});
+
+test("calculateDays", () => {
+  expect(calculateDays([5, 3, 0])).toEqual(3);
 });
