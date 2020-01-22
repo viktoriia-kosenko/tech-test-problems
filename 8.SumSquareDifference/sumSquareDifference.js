f32133b8bc440ab95d1febd65154fbe1ee7042ec
Vicky <kosenchiha@gmail.com>
2020-01-22 19:59:26 +0000

    Add solution for Sum square difference problem

--- 8.SumSquareDifference/sumSquareDifference.js
+++ 8.SumSquareDifference/sumSquareDifference.js
@@ -1,5 +1,11 @@
 const getSumSquareDifference = n => {
-  return 2640;
+  let sumOfSquare = 0;
+  let sumOfNumber = (n * (n + 1)) / 2;
+  for (let i = 1; i <= n; i++) {
+    sumOfSquare += Math.pow(i, 2);
+  }
+
+  return Math.pow(sumOfNumber, 2) - sumOfSquare;
 };
 
 module.exports = getSumSquareDifference;
--- 8.SumSquareDifference/sumSquareDifference.test.js
+++ 8.SumSquareDifference/sumSquareDifference.test.js
@@ -4,10 +4,10 @@ test("Find the difference between the sum of the squares of the first n natural 
   expect(getSumSquareDifference(10)).toEqual(2640);
 });
 
-test("Find the difference between the sum of the squares of the first n natural numbers and the square of the sum", () => {
-  expect(getSumSquareDifference(20)).toEqual(41230);
-});
+// test("Find the difference between the sum of the squares of the first n natural numbers and the square of the sum", () => {
+//   expect(getSumSquareDifference(20)).toEqual(41230);
+// });
 
-test("Find the difference between the sum of the squares of the first n natural numbers and the square of the sum", () => {
-  expect(getSumSquareDifference(100)).toEqual(25164150);
-});
+// test("Find the difference between the sum of the squares of the first n natural numbers and the square of the sum", () => {
+//   expect(getSumSquareDifference(100)).toEqual(25164150);
+// });
