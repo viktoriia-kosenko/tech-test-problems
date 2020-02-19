704e9f2fe1fb97004205609502825ae2d4090a82
Vicky <kosenchiha@gmail.com>
2020-02-19 22:24:00 +0000

    Add a case with an array of few levels

--- 9.NeedleInAHaystack/needleInAHaystack.test.js
+++ 9.NeedleInAHaystack/needleInAHaystack.test.js
@@ -4,12 +4,21 @@ describe("search", () => {
   test("should return true", () => {
     let result = search([["toe", "finger"], ["alien"]], "toe");
     expect(typeof result).toEqual("boolean");
-    expect(result).toBeTruthy();
+    expect(result).toBe(true);
+  });
+
+  test("should return true", () => {
+    let result = search(
+      [["finger"], ["toee", "alien", ["hello", ["hi", ["toe"]]]]],
+      "toe"
+    );
+    expect(typeof result).toEqual("boolean");
+    expect(result).toBe(true);
   });
 
   test("should return false", () => {
     let result = search([["electricity"]], "gas");
     expect(typeof result).toEqual("boolean");
-    expect(result).toBeFalsy();
+    expect(result).toBe(false);
   });
 });
