55689dce649d3fa53c107ba97949e770a4e47f77
Vicky <kosenchiha@gmail.com>
2020-02-19 22:27:42 +0000

    Add solution for needleInAHaystack problem

--- 9.NeedleInAHaystack/needleInAHaystack.js
+++ 9.NeedleInAHaystack/needleInAHaystack.js
@@ -1,5 +1,13 @@
 const search = (arr, word) => {
-  return 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string" && arr[i] === word) {
+      return true;
+    } else if (Array.isArray(arr[i])) {
+      let shortArr = [...arr[i], ...arr.slice(i + 1)];
+      return search(shortArr, word);
+    }
+  }
+  return false;
 };
 
 module.exports = search;
