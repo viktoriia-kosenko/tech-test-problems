318f2ceee10484524ea5c98f0092987cfc3058a8
Vicky <kosenchiha@gmail.com>
2020-03-29 22:40:56 +0100

    Add more test cases for anoter solution of the Sums Of Parts problem

--- 10.SumsOfParts/sumsOfParts.test.js
+++ 10.SumsOfParts/sumsOfParts.test.js
@@ -1,4 +1,4 @@
-const partsSums = require("./sumsOfParts");
+const { partsSums, sumsOfParts } = require("./sumsOfParts");
 
 describe("partsSums", () => {
   test("with empty array as an argument should return array with one element", () => {
@@ -13,6 +13,24 @@ describe("partsSums", () => {
 
   test("should return false", () => {
     let result = partsSums([1, 2, 3, 4, 5, 6]);
+    let expectedResult = [21, 20, 18, 15, 11, 6, 0];
+    expect(result).toEqual(expectedResult);
+  });
+});
+
+describe("sumsOfParts", () => {
+  test("with empty array as an argument should return array with one element", () => {
+    expect(sumsOfParts([])).toEqual([0]);
+  });
+
+  test("should return array of sums", () => {
+    let result = sumsOfParts([0, 1, 3, 6, 10]);
+    let expectedResult = [20, 20, 19, 16, 10, 0];
+    expect(result).toEqual(expectedResult);
+  });
+
+  test("should return false", () => {
+    let result = sumsOfParts([1, 2, 3, 4, 5, 6]);
     let expectedResult = [21, 20, 18, 15, 11, 6, 0];
     expect(result).toEqual(expectedResult);
   });
