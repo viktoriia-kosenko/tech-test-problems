14f9a5a3c125dcf0217749cf0b89a703173c14ee
Vicky <kosenchiha@gmail.com>
2020-04-01 23:07:11 +0100

    Add solution for Top Clients problem

---
+++ 13.TopClients/topClients.js
@@ -0,0 +1,24 @@
+function idBestUsers() {
+  let args = [...arguments];
+  let otherArgs = args.slice(1);
+  let clients = [...new Set(args[0])].filter(el =>
+    otherArgs.every(arr => arr.indexOf(el) >= 0)
+  );
+
+  if (clients.length === 0) {
+    return [];
+  } else {
+    let answer = [];
+    let allPurchases = [].concat(...args);
+    clients.sort().forEach(client => {
+      let num = allPurchases.filter(el => el === client).length;
+      let index = answer.findIndex(el => el[0] === num);
+      if (index >= 0) {
+        answer[index][1].push(client);
+      } else {
+        answer.push([num, [client]]);
+      }
+    });
+    return answer.sort((a, b) => b[0] - a[0]);
+  }
+}
